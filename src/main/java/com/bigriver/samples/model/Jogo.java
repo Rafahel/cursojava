31d0336a2629b528541adf790cd1323dfa168204
Douglas Liberalesso <dliberalesso@gmail.com>
2015-10-21 14:50:07 -0200

    Adiciona a classe Jogo.

---
+++ src/main/java/com/bigriver/samples/model/Jogo.java
@@ -0,0 +1,62 @@
+package com.bigriver.samples.model;
+
+public class Jogo {
+	
+	private int id;
+	private String nome, genero, plataforma;
+	private int jogadores;
+	private double preco;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public String getPlataforma() {
+		return plataforma;
+	}
+	
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+	public int getJogadores() {
+		return jogadores;
+	}
+	
+	public void setJogadores(int jogadores) {
+		this.jogadores = jogadores;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	@Override
+	public String toString() {
+		return nome;
+	}
+}
